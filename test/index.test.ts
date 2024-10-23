9f15f78edabf520d907ce516a169080423b54fcd
Julianne Quin Iaccarino <jiaccari@eceprog5.ecn.purdue.edu>
2024-10-23 09:58:56 -0400

    upate index.ts #38

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
 import { fetchContributorActivity, fetchRepoData, checkFolderExists, getReadmeDetails } from "../src/api/githubApi";
 import { getRepoDetails, extractDomainFromUrl, extractNpmPackageName, extractGithubOwnerAndRepo } from '../src/utils/urlHandler';
-import { calcBusFactorScore, calcCorrectnessScore, calcResponsivenessScore, calcLicenseScore, calculateMetrics, calcBusFactor, calcCorrectness, calcResponsiveness, calcLicense, calcRampUp } from "../src/metrics/metricCalcs";
+import { calcBusFactorScore, calcCorrectnessScore, calcResponsivenessScore, calcLicenseScore, calculateMetrics, calcBusFactor, calcCorrectness, calcResponsiveness, calcLicense, calcRampUp } from "../src/metrics/metric";
 import { initLogFile, logToFile, metricsLogToStdout } from "../src/utils/log";
 import * as path from 'path';
 import { runWorker } from '../src/index';
