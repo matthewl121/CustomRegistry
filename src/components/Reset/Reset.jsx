c8ef6813513625d68361a98f766c678cd6dd5072
beefcarhorn <aidan127535@gmail.com>
2024-10-28 17:48:54 -0400

    Added Reset button

---
+++ src/components/Reset/Reset.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Reset = () => {
+    const handleReset = () => {
+
+    }
+
+    return (
+        <button onClick={handleReset}>Reset</button>
+    )
+}
+
+export default Reset
