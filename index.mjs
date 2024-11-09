10924506be8bbf23eaa24c83aa7197233f7ce4e3
Matthew Li <mli007@hawaiiantel.net>
2024-11-09 15:36:13 -0500

    test

--- index.mjs
+++ index.mjs
@@ -1,11 +1,8 @@
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { spawn } from "child_process"; // Import child_process to run commands
+import { spawn, execSync } from "child_process"; // Import child_process to run commands
 
 const s3 = new S3Client({ region: "us-east-1" });
-
-const { execSync } = require('child_process');
-
 
 
 export const handler = async (event) => {
