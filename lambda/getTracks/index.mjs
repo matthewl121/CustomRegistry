d1c9116a7afc686c4fa441571723d8ece325130c
beefcarhorn <aidan127535@gmail.com>
2024-12-01 16:18:32 -0500

    changes

--- lambda/getTracks/index.mjs
+++ lambda/getTracks/index.mjs
@@ -1,20 +1,11 @@
 export const handler = async () => {
-    try {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          plannedTracks: [
-            "None"
-          ]
-        })
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: "The system encountered an error while retrieving the student's track information."
-        })
-      };
-    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        plannedTracks: [
+          "None"
+        ]
+      })
+    };
   };
   
