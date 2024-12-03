94cd588c3cc6a3e5af699451f5780bb2060c88dc
Julianne Quin Iaccarino <jiaccari@eceprog4.ecn.purdue.edu>
2024-12-03 15:55:59 -0500

    update ratePackage test &jest config

--- phase1/jest.config.js
+++ phase1/jest.config.js
@@ -20,24 +20,30 @@
 //     coverageReporters: ['text'], // Specify the format of the coverage report
 //   };
 
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // setupFilesAfterEnv: ['/home/shay/a/gscheng/ece461/ECE461/prototype_testing/jest.setup.ts'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.ts', '**/*.test.mjs'],
   collectCoverageFrom: [
-      // 'src/index.js',
-      // 'src/metricCalcs.js',
-      // 'src/api/apiUtils.js',
-      // 'src/api/githubApi.js',
       'src/api/graphqlQueries.js',
-      // 'src/api/npmApi.js',
       'src/utils/log.js',
       'src/utils/urlHandler/js',
       'src/utils/utils.js',
       'src/worker.js'
-    ],
-  collectCoverage: true, // Enable coverage collection
-  coverageDirectory: 'coverage', // Output directory for coverage reports
-  coverageReporters: ['text'], // Specify the format of the coverage report
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text'],
+  // Add these specific patterns
+  watchPathIgnorePatterns: ['**/cloudinary-*'],
+  modulePathIgnorePatterns: ['**/cloudinary-*'],
+  testPathIgnorePatterns: ['**/cloudinary-*'],
+  // Add this to handle temporary directories
+  haste: {
+      forceNodeFilesystemAPI: true,
+      throwOnModuleCollision: false,
+      ignoredFiles: ['**/cloudinary-*/**']
+  },
+  transform: {},
+  extensionsToTreatAsEsm: ['.mjs']
 };
