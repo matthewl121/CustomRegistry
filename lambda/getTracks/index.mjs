bef30ac3f5fb8ce593f58d26721ca2dade2bb637
Glen Shaofu Cheng <gscheng@eceprog1.ecn.purdue.edu>
2024-12-05 22:39:05 -0500

    Fix getTracks response

--- lambda/getTracks/index.mjs
+++ lambda/getTracks/index.mjs
@@ -1,12 +1,32 @@
 export const getTracksHandler = async () => {
+  try {
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({
         plannedTracks: [
         ]
       })
     };
-  };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: "The system encountered an error while retrieving the student's track information." }),
+    };
+  }
+};
 
   // TDOD UPDATE TEST TO CHECK EMPTY OBJECT
   
